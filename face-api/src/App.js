0c593821f8234feb0e779d7cc87cd2a7ecf5dbd7
Bryce-Soghigian <brybrycodingguy@gmail.com>
2019-12-31 00:27:39 -0600

    sketch logic

--- face-api/src/App.js
+++ face-api/src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
+import objectDetectionSketch from './ObjectDetectionSketch';
+
+import P5Wrapper from 'react-p5-wrapper';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <P5Wrapper sketch={objectDetectionSketch} />
     </div>
   );
 }
